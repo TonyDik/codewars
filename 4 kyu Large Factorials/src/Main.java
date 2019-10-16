b53c5dc147216927ae57ec5eaeff7af3b7b23e3d
Anton Dikarev <a.dikarev@abcconsulting.ru>
2019-10-16 12:38:28 +0300

    add large factorial kata

---
+++ 4 kyu Large Factorials/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = -2; i < 100; i++) {
+            System.out.println(Kata.Factorial(i));
+            System.out.println();
+        }
+//        Kata kata = new Kata();
+//        kata.multiply(3, Arrays.asList(1,2,3));
+//        Kata.Factorial(100);
+    }
+}
